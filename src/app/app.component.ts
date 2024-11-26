d7989633386dcc38fac56cb0da55f6971faab5aa
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-26 23:39:25 +0000

    Refactor AppComponent to use inline template and include header/footer components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,34 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { AppHeaderComponent } from "./app-header/app-header.component";
+import { AppFooterComponent } from "./app-footer/app-footer.component";
+import { RouterLink, RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  template: `
+    <app-header></app-header>
+    <section>
+      <h1>{{ title }}</h1>
+      <p>Welcome to the Rugby Score Card App</p>
+
+      <p>Use the navigation links above to get started</p>
+
+      <p>Click on the 'Home' link to return to this page</p>
+      <nav>
+        <ul>
+          <li><a routerLink="/home">Home</a></li>
+          <li><a routerLink="/calendar">Calendar</a></li>
+          <li><a routerLink="/score-tracker">Score Tracker</a></li>
+        </ul>
+      </nav>
+      <router-outlet />
+    </section>
+
+    <app-footer></app-footer>
+  `,
+  imports: [AppHeaderComponent, AppFooterComponent, RouterLink, RouterOutlet],
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rugby-score-card-app';
