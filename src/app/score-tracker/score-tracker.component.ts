97afa22cb2b7285de82fd8f6d2f0416bb6343ee2
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-26 15:31:23 +0000

    Add ScoreTracker component with initial template and test suite

---
+++ src/app/score-tracker/score-tracker.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-score-tracker',
+  imports: [],
+  templateUrl: './score-tracker.component.html',
+  styleUrl: './score-tracker.component.scss'
+})
+export class ScoreTrackerComponent {
+
+}
