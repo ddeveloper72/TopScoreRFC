9f93652a29a8944303011ec744d23776ff54941d
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-26 23:41:56 +0000

    Add AppFooter component with template and styles

---
+++ src/app/app-footer/app-footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <footer>
+      <p>&copy; 2024 Rugby Score Card</p>
+    </footer>
+    `,
+  styleUrl: './app-footer.component.scss'
+})
+export class AppFooterComponent {
+
+}
