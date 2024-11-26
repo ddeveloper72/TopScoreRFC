929e12c119c4d3e20b6be3a9469e5bfc6241ed61
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-26 15:56:24 +0000

    Add routing configuration for ScoreTracker and Calendar components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { ScoreTrackerComponent } from './score-tracker/score-tracker.component';
+import { CalendarComponent } from './calendar/calendar.component';
 
-export const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'score-tracker', pathMatch: 'full' },
+  { path : 'score-tracker', component: ScoreTrackerComponent },
+  { path: 'calendar', component: CalendarComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
