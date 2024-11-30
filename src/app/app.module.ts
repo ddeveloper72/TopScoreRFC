5cfbb6d6d0867d2fa7f8c96f9799ecf669b7000a
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-30 22:15:25 +0000

    Refactor app module to include AppComponent in imports for proper component registration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AngularFireModule } from '@angular/fire/compat';
 import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { environment } from '../environments/environment';
 import { AppRoutingModule } from './app.routes';
 import { RouterModule } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { AppComponent } from './app.component';
 
 
 @NgModule({
-  declarations: [
-  ],
+  declarations: [],
   imports: [
+    AppComponent,
     BrowserModule,
+    AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFirestoreModule,
-    AppRoutingModule,
     RouterModule,
   ],
+  exports: [],
   providers: [],
 })
 export class AppModule {}
