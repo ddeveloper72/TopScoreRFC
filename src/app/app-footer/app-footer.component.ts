a3b32169664c2656ed511a0c99d96eada876b810
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-30 10:18:54 +0000

    Refactor app-footer.component.ts to use external template file for improved maintainability

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -3,11 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-footer',
   imports: [],
-  template: `
-    <footer>
-      <p>&copy; 2024 Rugby Score Card</p>
-    </footer>
-    `,
+  templateUrl: './app-footer.component.html',
   styleUrl: './app-footer.component.scss'
 })
 export class AppFooterComponent {
