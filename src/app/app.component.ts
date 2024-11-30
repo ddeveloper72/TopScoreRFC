e79ad71fb114a88aca80628ac5696cac7cca2f59
ddeveloper72 <duncan.niamh@gmail.com>
2024-11-30 22:15:16 +0000

    Refactor app component to include MaterialModule and SidenavListComponent for enhanced navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
-import { AppHeaderComponent } from "./app-header/app-header.component";
-import { AppFooterComponent } from "./app-footer/app-footer.component";
+import { AppHeaderComponent } from './navigation/app-header/app-header.component';
+import { AppFooterComponent } from './app-footer/app-footer.component';
 import { RouterLink, RouterOutlet } from '@angular/router';
-
+import { MaterialModule } from './material/material.module';
+import { ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material/sidenav';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  imports: [AppHeaderComponent, AppFooterComponent, RouterLink, RouterOutlet],
+  imports: [
+    AppHeaderComponent,
+    AppFooterComponent,
+    RouterLink,
+    RouterOutlet,
+    MaterialModule,
+    SidenavListComponent
+  ],
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'rugby-score-card-app';
+
+  @ViewChild('sidenav')
+  sidenav: MatSidenav = new MatSidenav();
 }
