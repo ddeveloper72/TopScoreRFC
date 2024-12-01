5cfac77c05752d8267ce7332d9506d42270d551b
ddeveloper72 <duncan.niamh@gmail.com>
2024-12-01 12:17:00 +0000

    WIP - Add score tracking functionality to ScoreTrackerComponent

--- src/app/score-tracker/score-tracker.component.ts
+++ src/app/score-tracker/score-tracker.component.ts
@@ -1,11 +1,50 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-score-tracker',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './score-tracker.component.html',
   styleUrl: './score-tracker.component.scss'
 })
-export class ScoreTrackerComponent {
 
+export class ScoreTrackerComponent {
+  matchName: string = '';
+  matchDate: Date | null = null;
+  isHomeGame: boolean = true;
+  ourTeamScore = {
+    tries: 0,
+    conversions: 0,
+    penalties: 0,
+    scorers: {
+      firstHalf: [] as string[],
+      secondHalf: [] as string[]
+    }
+  };
+  opponentTeamScore = {
+    tries: 0,
+    conversions: 0,
+    penalties: 0
+  };
+
+  get totalOurTeamPoints(): number {
+    return (this.ourTeamScore.tries * 5) + (this.ourTeamScore.conversions * 2) + (this.ourTeamScore.penalties * 3);
+  }
+
+  get totalOpponentTeamPoints(): number {
+    return (this.opponentTeamScore.tries * 5) + (this.opponentTeamScore.conversions * 2) + (this.opponentTeamScore.penalties * 3);
+  }
+
+  generatePointsReport(): string {
+    return `
+      Match: ${this.matchName}
+      Date: ${this.matchDate?.toLocaleDateString()}
+      Home Game: ${this.isHomeGame ? 'Yes' : 'No'}
+      Our Team Points: ${this.totalOurTeamPoints}
+      Opponent Team Points: ${this.totalOpponentTeamPoints}
+      Our Team Scorers (First Half): ${this.ourTeamScore.scorers.firstHalf.join(', ')}
+      Our Team Scorers (Second Half): ${this.ourTeamScore.scorers.secondHalf.join(', ')}
+    `;
+  }
 }
