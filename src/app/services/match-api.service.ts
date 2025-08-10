832f60a67887ed3b24936713c96e02dfcb373052
Duncan Falconer <duncan.niamh@gmail.com>
2025-08-10 15:51:47 +0100

    Refactor MatchApiService to utilize AppConfigService for dynamic API URL configuration

--- src/app/services/match-api.service.ts
+++ src/app/services/match-api.service.ts
@@ -3,16 +3,20 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { environment } from '../../environments/environment';
+import { AppConfigService } from './app-config.service';
 import { Match } from './match-storage.service';
 
 @Injectable({ providedIn: 'root' })
 export class MatchApiService {
-  private apiUrl = environment.apiUrl || 'http://localhost:3000/api';
+  private apiUrl: string;
   private httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' }),
   };
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private cfg: AppConfigService) {
+    this.apiUrl =
+      this.cfg.apiUrl || environment.apiUrl || 'http://localhost:3000/api';
+  }
 
   getAllMatches(): Observable<Match[]> {
     return this.http.get<any[]>(`${this.apiUrl}/matches`).pipe(
