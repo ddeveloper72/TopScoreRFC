3caee10165d1c752ac2d50a3e620661c420a2af8
ddeveloper72 <ddeveloper72@users.noreply.github.com>
2025-08-11 00:26:15 +0000

    deploy: 53b45091f8123d0781765e2c94d5b3112ca3fa84

--- assets/env.js
+++ assets/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = 'https://rugbyappbackend-4014b68ac4bb.herokuapp.com';
+  window.__env.apiUrl = 'https://rugbyappbackend-4014b68ac4bb.herokuapp.com/api';
   window.__env.googleMaps = { apiKey: '', mapId: '' };
 })(this);
