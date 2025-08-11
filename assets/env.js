6f6f71862b335bdf539a15b4a55737a412552647
ddeveloper72 <ddeveloper72@users.noreply.github.com>
2025-08-11 22:34:21 +0000

    deploy: 16230aff25e2754317e9f05bceca1aeb225d837f

--- assets/env.js
+++ assets/env.js
@@ -1,5 +1,5 @@
 (function (window) {
   window.__env = window.__env || {};
-  window.__env.apiUrl = 'https://rugbyappbackend-4014b68ac4bb.herokuapp.com/api';
-  window.__env.googleMaps = { apiKey: '', mapId: '' };
+  window.__env.apiUrl = 'https://rugbyappbackend-4014b68ac4bb.herokuapp.com/';
+  window.__env.googleMaps = { apiKey: 'AIzaSyC-Kp2v6t3NlEh7AvzdxFSj-JLRf-UMyGg', mapId: '' };
 })(this);
