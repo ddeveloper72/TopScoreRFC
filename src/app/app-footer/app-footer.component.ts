77dd39530544ecf43782d29b470680397507da2b
Duncan Falconer <duncan.niamh@gmail.com>
2025-08-31 18:00:50 +0100

    style: add MaterialModule import and implement openAboutModal method in footer component

--- src/app/app-footer/app-footer.component.ts
+++ src/app/app-footer/app-footer.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material/dialog';
+import { MaterialModule } from '../material/material.module';
+import { AboutModalComponent } from './about-modal/about-modal.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [MaterialModule],
   templateUrl: './app-footer.component.html',
-  styleUrl: './app-footer.component.scss'
+  styleUrl: './app-footer.component.scss',
 })
 export class AppFooterComponent {
+  constructor(private dialog: MatDialog) {}
 
+  openAboutModal(): void {
+    this.dialog.open(AboutModalComponent, {
+      width: '500px',
+      maxWidth: '90vw',
+      panelClass: 'about-modal',
+    });
+  }
 }
